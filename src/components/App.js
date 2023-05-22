d326ed01c45c4302dfd83f473e51ea641bb80573
GaneshChaturthiCJ <67580297+GaneshChaturthiCJ@users.noreply.github.com>
2023-05-22 11:40:45 +0530

    Update App.js
    added code

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,52 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 
 const App = () => {
-  const [rating, setRating] = useState();
-  const [comment, setComment] = useState();
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState(true);
   const [commentError, setCommentError] = useState();
+  
+  
+  const ratingUpdate = (event) =>{
+      setRating(event.target.value);
+  }
+  const validation = (event) =>{
+    if(event.target.value.length < 5 ){
+      setComment(false);
+    } else {
+      setComment(true)
+    }
+  }
+  
+  const preventDefault = (event) =>{
+    event.preventDefault();
+  }
 
   return (
     <div id="main">
-      <form>
+      <form onSubmit={preventDefault}>
         <div>
           <label htmlFor='rating'>Rating: </label>
           <input 
             type="range"
             min="1"
             max="10"
-            id="rating"
+            id="rating" 
+            onChange={ratingUpdate}
           />
-          <span className='rating'>rating</span>
+          <span className='rating'>{rating}</span>
         </div>
         <div>
           <label htmlFor='comment'>Comment: </label>
           <textarea 
             id='comment'
+            onChange={validation}
           />
-          <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>
+          {comment ? "" : <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>}
         </div>
-        <button type='submit'>Submit</button>
+        <button type='submit' >Submit</button>
       </form>
     </div>
   )
 }
-
 
 export default App;
